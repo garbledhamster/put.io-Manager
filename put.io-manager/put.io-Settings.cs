520585910ff7af2277f5b29623bb7b803d597b3a
Joe Rice <jrice@kcsupport.net>
2018-05-01 16:52:55 -0500

    fixed an issue that occured when trying to push changes.

--- put.io-manager/put.io-Settings.cs
+++ put.io-manager/put.io-Settings.cs
@@ -6,7 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using System.Net.htt
 namespace put.io_manager
 {
     public partial class FormSettings : Form
@@ -23,7 +23,8 @@ namespace put.io_manager
 
         private void buttonGetToken_Click(object sender, EventArgs e)
         {
-            
+           string requestURL = Authentication.OAuthRequestURL("3355", "oob", "na");
+
         }
     }
 }
